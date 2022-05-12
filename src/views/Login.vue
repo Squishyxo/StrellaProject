f6cb733e03c07f541dd936f47d3b7912bcb01e82
Mohammed Alshukaili <m.aalshukaili@student.fontys.nl>
2022-05-12 16:29:11 +0400

    designed log in page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="login">
       <div class="left">
+          <img class="login-logo" src="../images/balenciaga-dark-mode-logo.png" alt="">
           <form>
               <h2>Log in</h2>
               <input type="password" placeholder="Access Token">
               <p>Forgot Token</p>
-              <button @click="logIn"><router-link to="/">Sign in</router-link></button>
+              <router-link to="/"><button @click="logIn">Sign in</button></router-link>
           </form>
       </div>
       <div class="right"><img src="../images/Login.jpg" alt=""></div>
@@ -18,7 +19,7 @@ export default {
 }
 </script>
 
-<style>
+<style scpoed>
 .login{
     display: flex;
     align-items: center;
@@ -31,7 +32,38 @@ export default {
     height: 100vh;
     padding: 20px 120px;
 }
+.login-logo{
+    width: 50%;
+    padding: 6rem 0;
+}
 form{
     border: 1px solid yellow;
+    margin-top: 7rem;
+    width: 80%;
+}
+form p{
+    cursor: pointer;
+    display: flex;
+    justify-content: end;
+}
+h2{
+    font-size: 2rem;
+}
+input{
+    width: 40%;
+    height: 4vh;
+    border-radius: 7px;
+    border: 1px solid gray;
+    padding: 0 10px;
+    color: rgb(0, 0, 0);
+    font-weight: 600;
+}
+button{
+    width: 40%;
+    height: 4vh;
+    background-color: #0D161C;
+    color: white;
+    cursor: pointer;
+    border-radius: 7px;
 }
 </style>
